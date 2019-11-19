038d710e24e60b88e082575d9066db59fe86ae74
nem <evgeny.nikitjuk@soft-werke.com>
2019-11-19 14:13:39 +0300

    fix cycle dependency

--- library/src/main/java/ru/otus/spring/library/repository/impl/CommentDaoImpl.java
+++ library/src/main/java/ru/otus/spring/library/repository/impl/CommentDaoImpl.java
@@ -1,24 +1,34 @@
 package ru.otus.spring.library.repository.impl;
 
 import org.hibernate.Hibernate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.otus.spring.library.domain.Comment;
-import ru.otus.spring.library.repository.CommentDao;
 import ru.otus.spring.library.repository.CommentDaoCustom;
 import ru.otus.spring.library.repository.JpaRepositoryException;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@SuppressWarnings("JpaQlInspection")
 @Repository
 @Transactional(readOnly = true)
 public class CommentDaoImpl implements CommentDaoCustom {
-    @Autowired
-    CommentDao commentDao;
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public Comment getCommentByIdWithBook(long commentId) throws JpaRepositoryException {
-        Comment comment = commentDao.findById(commentId).orElseThrow(() -> new JpaRepositoryException("Returned comment is null"));
-        Hibernate.initialize(comment.getBook());
-        return comment;
+        TypedQuery<Comment> query = em.createQuery("select c from Comment c where c.id = :commentId", Comment.class);
+        query.setParameter("commentId", commentId);
+        try {
+            Comment comment = query.getSingleResult();
+            Hibernate.initialize(comment.getBook());
+            return comment;
+        } catch (Exception e) {
+            throw new JpaRepositoryException("Returned comment is null");
+        }
     }
 }
