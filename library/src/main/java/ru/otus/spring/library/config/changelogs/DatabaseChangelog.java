986c641ecd6edb11f5783648e0553650b9e50a6c
Evgeny Nikityuk <deyk94@yandex.ru>
2019-12-08 21:34:47 +0300

    add tests

--- library/src/main/java/ru/otus/spring/library/config/changelogs/DatabaseChangelog.java
+++ library/src/main/java/ru/otus/spring/library/config/changelogs/DatabaseChangelog.java
@@ -2,7 +2,6 @@ package ru.otus.spring.library.config.changelogs;
 
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import ru.otus.spring.library.domain.Author;
 import ru.otus.spring.library.domain.Book;
@@ -16,29 +15,16 @@ import java.util.Date;
 public class DatabaseChangelog {
     @ChangeSet(order = "001", id = "initData", author = "databaseChangelog")
     public void initData(MongoTemplate mongoTemplate) {
-        Author author_1 = mongoTemplate.save(new Author("author_1"));
-        Author author_2 = mongoTemplate.save(new Author("author_2"));
-        Author author_3 = mongoTemplate.save(new Author("author_3"));
+        Author author_1 = mongoTemplate.save(new Author("author_01"));
+        Author author_2 = mongoTemplate.save(new Author("author_02"));
+        Author author_3 = mongoTemplate.save(new Author("author_03"));
 
-        Comment comment_1 = mongoTemplate.save(new Comment("comment_1", new Date()));
-        Comment comment_2 = mongoTemplate.save(new Comment("comment_2", new Date()));
-        Comment comment_3 = mongoTemplate.save(new Comment("comment_3", new Date()));
+        Comment comment_1 = mongoTemplate.save(new Comment("comment_01", new Date()));
+        Comment comment_2 = mongoTemplate.save(new Comment("comment_02", new Date()));
+        Comment comment_3 = mongoTemplate.save(new Comment("comment_03", new Date()));
 
-        mongoTemplate.save(new Book("book_1", Collections.singletonList(author_1), Collections.singletonList(comment_1)));
-        mongoTemplate.save(new Book("book_2", Arrays.asList(author_1, author_2), Arrays.asList(comment_1, comment_2)));
-        mongoTemplate.save(new Book("book_3", Arrays.asList(author_1, author_2, author_3), Arrays.asList(comment_1, comment_2, comment_3)));
-    }
-
-    @ChangeSet(order = "002", id = "initTestData", author = "databaseChangelog")
-    @Profile("test")
-    public void initTestData(MongoTemplate mongoTemplate) {
-        Author author_1 = mongoTemplate.save(new Author("author_1"));
-        Author author_2 = mongoTemplate.save(new Author("author_2"));
-
-        Comment comment_1 = mongoTemplate.save(new Comment("comment_1", new Date()));
-        Comment comment_2 = mongoTemplate.save(new Comment("comment_2", new Date()));
-
-        mongoTemplate.save(new Book("book_1", Collections.singletonList(author_1), Collections.singletonList(comment_1)));
-        mongoTemplate.save(new Book("book_2", Arrays.asList(author_1, author_2), Arrays.asList(comment_1, comment_2)));
+        mongoTemplate.save(new Book("book_01", Collections.singletonList(author_1), Collections.singletonList(comment_1)));
+        mongoTemplate.save(new Book("book_02", Arrays.asList(author_1, author_2), Arrays.asList(comment_1, comment_2)));
+        mongoTemplate.save(new Book("book_03", Arrays.asList(author_1, author_2, author_3), Arrays.asList(comment_1, comment_2, comment_3)));
     }
 }
